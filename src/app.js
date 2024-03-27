4ec3ff073b167439b7a17d745027a3936b446a86
Zoe Kyner <kynerzoe@gmail.com>
2024-03-27 16:27:09 -0600

    added movies routing to app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,29 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
-const cors = require('cors');
 const app = express();
+const cors = require("cors");
+const moviesRouter = require('./movies/movies.router');
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/movies', moviesRouter);
+
+//Error Handler
+app.use((req, res, next) => {
+    next({
+        status: 500,
+        message: 'Something went wrong!'
+    });
+});
+
+//Not Found Handler
+app.use((req, res, next) => {
+    next({
+      status: 404,
+      message: `Not found: ${req.originalUrl}`,
+    });
+});
+
 
 module.exports = app;
