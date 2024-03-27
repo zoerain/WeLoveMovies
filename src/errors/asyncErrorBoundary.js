c76281130c44dd7a54ed9aa1e462742250069cf6
Zoe Kyner <kynerzoe@gmail.com>
2024-03-27 17:34:32 -0600

    added error handlers

---
+++ src/errors/asyncErrorBoundary.js
@@ -0,0 +1,11 @@
+function asyncErrorBoundary(handler) {
+  return async (req, res, next) => {
+    try {
+      await handler(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+module.exports = asyncErrorBoundary;
